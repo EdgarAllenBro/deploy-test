40c73dee75a6407367bf6450148ef74aa57647d6
Cameron Grieder <cjgrieder@gmail.com>
2022-01-12 10:13:52 -0700

    fixed some issues

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const path = require('path')
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'../index.html'));
+    res.sendFile(path.join(__dirname,'/index.html'));
 });
+app.use(express.static('public'))
 
 const port = process.env.PORT || 4005
 
